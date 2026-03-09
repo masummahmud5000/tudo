c54fd5bc8b6f4bf25a1de4d75ec7718d7a0b6d57
mdmasum <mdmasumpresent@gmail.com>
2026-03-09 22:17:20 +0600

    ready

--- tudo/app/_serverActions/axios.ts
+++ tudo/app/_serverActions/axios.ts
@@ -1,3 +1,6 @@
 import axios from "axios";
 
-export const api = axios.create({baseURL: 'https://render-6-pg2u.onrender.com/'});
+export const api = axios.create({
+    baseURL: 'https://render-6-pg2u.onrender.com/', 
+    // baseURL:'http://127.0.0.1:8000/'
+});
