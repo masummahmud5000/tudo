c27412e6380da79f202ac085a36c4835e25cf3e2
masummahmud5000 <mdmasumpresent@gmail.com>
2026-03-12 20:19:20 +0600

    g

--- tudo/app/_serverActions/axios.ts
+++ tudo/app/_serverActions/axios.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'https://render-6-pg2u.onrender.com/',
+    baseURL: proccess.env.NEXT_PUBLIC_API_URL;
+    //baseURL: 'https://render-6-pg2u.onrender.com/',
     // baseURL:'http://127.0.0.1:8000/'
 });
